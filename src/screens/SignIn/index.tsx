418e97764a3bcab11dc232d6bc0bd857115ab9ad
Carina Yanaze <carinaoyanaze@gmail.com>
2022-01-12 15:59:35 -0300

    desafio pronto

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -28,6 +28,14 @@ export function SignIn() {
   // creates a function to handle sign in
     // try to call and wait signIn
     // if fails, display an Alert with the title "Erro SignIn" and message "Ocorreu um erro ao tentar logar no app"
+  async function handleSignIn(){
+    try{
+      await signIn();
+    } catch (error) {
+      Alert.alert('Erro SignIn', 'Ocorreu um erro ao tentar logar no app');
+      
+    }
+  }
 
   return (
     <Container
@@ -61,19 +69,18 @@ export function SignIn() {
             o mundo da Twitch
           </Description>
 
-          {/* <SignInButton onPress={}>
+          <SignInButton onPress={handleSignIn}>
             <SignInButtonIcon>
-              Verify if isLoggingIn is true
-              If it is, show an ActivityIndicator
-              Otherwise, show Fontisto's twitch icon
+              { isLoggingIn ? <ActivityIndicator color={theme.colors.white} style={{width: 20, }} /> :
+                <Fontisto name="twitch" style={{width:20, marginRight:1}} color={theme.colors.white}/>
+              }
             </SignInButtonIcon>
 
             <SignInButtonText>
-              Verify if isLoggingIn is true
-              If it is, show "Entrando..."
-              Otherwise, show "Entrar com Twitch"
+              { isLoggingIn ? 'Entrando...' : 'Entrar com Twitch'
+              }
             </SignInButtonText>
-          </SignInButton> */}
+          </SignInButton>
         </LoginInfo>
       </Content>
 
